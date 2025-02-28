f8c976683440511cf7ccc5e88f585c30392f6d2a
david-william-l <davidwillreis3@gmail.com>
2025-02-28 20:45:39 -0300

    shield

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -6,7 +6,7 @@ use CodeIgniter\Config\BaseConfig;
 
 class Email extends BaseConfig
 {
-    public string $fromEmail  = '';
+    public string $fromEmail  = 'davidwillreis3@gmail.com';
     public string $fromName   = '';
     public string $recipients = '';
 

adc0f198dc3677dc94596ed48e70f4cbd119a602
david-william-l <davidwillreis3@gmail.com>
2025-02-28 21:09:14 -0300

    vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,47 @@
+<?php return array(
+    'root' => array(
+        'name' => 'david/projeto-e-jobs',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '3531a57d446c8960512261f6fc263b0e0fbc60e3',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'codeigniter4/authentication-implementation' => array(
+            'dev_requirement' => false,
+            'provided' => array(
+                0 => '1.0',
+            ),
+        ),
+        'codeigniter4/settings' => array(
+            'pretty_version' => 'v2.2.0',
+            'version' => '2.2.0.0',
+            'reference' => '2748f2b4572d44a940f98c31847d65272cac5666',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../codeigniter4/settings',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'codeigniter4/shield' => array(
+            'pretty_version' => 'v1.1.0',
+            'version' => '1.1.0.0',
+            'reference' => '22a8b3b58dafa7a5c080bc61446653aeb9fffc06',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../codeigniter4/shield',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'david/projeto-e-jobs' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '3531a57d446c8960512261f6fc263b0e0fbc60e3',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);

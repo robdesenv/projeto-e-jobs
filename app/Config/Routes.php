153a05464234092e3bdab8ae7da8a49dc5765c6d
david-william-l <davidwillreis3@gmail.com>
2025-03-14 19:07:29 -0300

    Filtro de rotas

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,8 +6,8 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-$routes->get('/freelancer', 'FreelancerController::index');
-$routes->get('/contratante', 'ContratanteController::index');
+$routes->get('/freelancer', 'FreelancerController::index', ['filter' => 'group:freelancer']);
+$routes->get('/contratante', 'ContratanteController::index', ['filter' => 'group:contratante']);
 
 
 service('auth')->routes($routes);
